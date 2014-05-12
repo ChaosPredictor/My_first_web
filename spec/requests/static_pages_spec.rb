f5c3acc9ce4e5df1395968b5342c4007782c4aa2
ChaosPredictor <kuzi81@gmail.com>
2014-05-12 15:49:02 +0300

    Pretty RSpec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,59 +3,31 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home page" do
-
-    it "should have the content 'Chaos predictor'" do
-      visit root_path
-      expect(page).to have_content('This site was opened without')
-    end
+	before { visit root_path }
 	
-	it "should have the title 'Home'" do
-      visit root_path
-      expect(page).to have_title("My First Web")
-    end
-	
-	it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('|')
-    end
+	it { expect(page).to have_content('This site was opened without') }
+	it { expect(page).to have_title(full_title('')) }
+	it { expect(page).not_to have_title('|') }
   end
   
   describe "Help page" do
+	before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-	
-	it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("My First Web | Help")
-    end
+    it { expect(page).to have_content('Help') }
+	it { expect(page).to have_title(full_title('Help')) }
   end
 
   describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
+	before { visit about_path }
 	
-	it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("My First Web | About Us")
-    end
+    it { expect(page).to have_content('About Us') }
+	it { expect(page).to have_title(full_title('About Us')) }
   end 
 
   describe "Contact page" do
-
-    it "should have the content 'kuzi81@gmail.com'" do
-      visit contact_path
-      expect(page).to have_content('kuzi81@gmail.com')
-    end
-	
-	it "should have the title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title("| Contact")
-    end
+	before { visit contact_path }	
+  
+    it { expect(page).to have_content('kuzi81@gmail.com') }
+	it { expect(page).to have_title(full_title('Contact')) }
   end   
 end
