f9eb28b8cfa9d963d7aa94072f437f4ca7041213
ChaosPredictor <kuzi81@gmail.com>
2014-05-14 23:16:17 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      # Handle a successful save.
+	  flash[:success] = "Welcome to the My First Web!"
+      redirect_to @user
     else
       render 'new'
     end
