7010ff5966b871ff0a128d273e916bb98ced4268
ChaosPredictor <kuzi81@gmail.com>
2014-05-16 17:48:17 +0300

    signout done

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,8 +2,8 @@ module SessionsHelper
 
   def sign_in(user)
     remember_token = User.new_remember_token
-    #cookies.permanent[:remember_token] = remember_token
-	cookies.permanent[:remember_token] = { value:   remember_token, expires: 20.years.from_now.utc }
+    cookies.permanent[:remember_token] = remember_token
+	#cookies.permanent[:remember_token] = { value:   remember_token, expires: 20.years.from_now.utc }
     user.update_attribute(:remember_token, User.hash(remember_token))
     self.current_user = user
   end
@@ -19,5 +19,11 @@ module SessionsHelper
   
   def signed_in?
     !current_user.nil?
+  end
+  
+  def sign_out
+    current_user.update_attribute(:remember_token, User.hash(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
   end
 end
